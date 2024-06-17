eb58fa849b2a408f078e84c29e5b0f66b048275b
ethbergMuzola <ethbergmuzola500@gmail.com>
2024-06-17 15:33:33 +0200

    Fix: correction bug corse

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VueDevTools()],
-  server: {
-    port: 5067
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+module.exports = {
+  devServer: {
+    proxy: 'http://213.136.74.84:800/'
+  }
+}
