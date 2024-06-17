3b733646149f005f1a308c310bfa73f82feede2b
ethbergMuzola <ethbergmuzola500@gmail.com>
2024-06-17 15:58:24 +0200

    Fix: correction bug cors policy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 /* eslint-disable no-undef */
 module.exports = {
   devServer: {
-    proxy: 'http://213.136.74.84:800/'
+    proxy: {
+      '^/api': {
+        target: 'http://213.136.74.84:800',
+        ws: true,
+        changeOrigin: true
+      }
+    }
   }
 }
