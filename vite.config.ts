4afc1c28fdccb400d7f9cd5b61920d2a84e1ab98
ethbergMuzola <ethbergmuzola500@gmail.com>
2024-06-17 15:27:37 +0200

    Fix: correction bug corse

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), VueDevTools()],
+  server: {
+    port: 5067
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
