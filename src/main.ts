2d00ba7b09cd8a7a7bb97af1bf4447b99df76420
ethbergMuzola <ethbergmuzola500@gmail.com>
2024-07-08 15:35:52 +0200

    Feat: Add package fontAwesome for icon

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import '@progress/kendo-theme-default/dist/all.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import App from './App.vue'
 import router from './router'
 
