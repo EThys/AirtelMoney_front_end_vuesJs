14b5e9d709732d09782b7246b0dc3dd8d5edf06f
ethbergMuzola <ethbergmuzola500@gmail.com>
2024-07-08 15:37:37 +0200

    Feat: Add new Login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/public/HomeView.vue'
-import Login from '@/views/public/auth/LoginView.vue'
 import Profile from '@/views/public/ProfileView.vue'
 import NotFound from '@/views/public/NotFoundView.vue'
 import TransactionView from '@/views/public/transactions/TransactionView.vue'
-import originalView from '@/views/public/transactions/originalView.vue'
 import OtpView from '@/views/public/auth/OtpView.vue'
-// import AddTransactionView from '@/views/public/transactions/AddTransactionModalView.vue'
+import AuthLoginView from '@/views/public/auth/AuthLoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'login', component: Login },
+    { path: '/', name: 'AuthLoginView', component: AuthLoginView },
     { path: '/profile', name: 'profile', component: Profile },
     { path: '/currency/:currency', name: 'currency', component: TransactionView },
     { path: '/otp', name: 'otp', component: OtpView },
-    { path: '/currency/eth', name: 'eth', component: originalView },
+    // { path: '/currency/eth', name: 'AuthLoginView', component: AuthLoginView },
     { path: '/:pathMatch(.*)*', component: NotFound }
     // { path: '/home', name: 'home', component: Home }, // component: () => import('../views/AboutView.vue')
-    // { path: '/currency/currencyCode:', name: 'currency', component: Profile },
   ]
 })
 
